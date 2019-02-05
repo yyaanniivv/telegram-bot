5daa7d834234bcc60221368b5311ab13997d944a
Yaniv M <yanivmichaelis@users.noreply.github.com>
2019-02-05 23:23:03 +0200

    WIP, classes

--- src/bot.py
+++ src/bot.py
@@ -24,73 +24,82 @@ TELEGRAM_LOGGER = "TelegramBot"
 
 
 # Will lookup the list of magnets.
-class InlineHandler(InlineUserHandler, AnswererMixin):
-    def __init__(self, *args, **kwargs):
-        super(InlineHandler, self).__init__(*args, **kwargs)
-
-    def on_inline_query(self, msg):
-        def compute_answer():
-            query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
-            # print(self.id, ':', 'Inline Query:', query_id, from_id, query_string)
-            articles = create_magnet_articles(query_string)
-            return articles
-
-        self.answerer.answer(msg, compute_answer)
-
-    def on_chosen_inline_result(self, msg):
-        result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
-        print(self.id, ':', 'Chosen Inline Result:', result_id, from_id, query_string)
+# class InlineHandler(InlineUserHandler, AnswererMixin):
+#     def __init__(self, *args, **kwargs):
+#         super(InlineHandler, self).__init__(*args, **kwargs)
+#
+#     def on_inline_query(self, msg):
+#         def compute_answer():
+#             query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+#             # print(self.id, ':', 'Inline Query:', query_id, from_id, query_string)
+#             articles = Bot.create_magnet_articles(query_string)
+#             return articles
+#
+#         self.answerer.answer(msg, compute_answer)
+#
+#     def on_chosen_inline_result(self, msg):
+#         result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
+#         print(self.id, ':', 'Chosen Inline Result:', result_id, from_id, query_string)
 
 
 # Accepts magnet -> results in file in OUTPUT_DIR path.
-class DirectMsgHandler(telepot.aio.helper.ChatHandler):
+# class DirectMsgHandler(telepot.aio.helper.ChatHandler):
+#     def __init__(self, *args, **kwargs):
+#         super(DirectMsgHandler, self).__init__(*args, **kwargs)
+#         self.logger = logging.getLogger(TELEGRAM_LOGGER)
+#
+#     async def on_chat_message(self, msg):
+#         sender_id = msg['from']['id']
+#         self.logger.debug('sender_id: ' + str(sender_id))
+#         self.logger.info("Incoming message:")
+#         self.logger.info(msg)
+#
+#         if str(sender_id) in self.approved_ids:
+#             self.logger.debug('Known user sent a message.')
+#
+#             command = msg['text']
+#             self.logger.debug('Command is ' + command)
+#
+#             if 'magnet' in command:
+#                 create_torrent(msg['text'], msg['date'])
+#                 self.logger.info(str(sender_id) + ' ' + msg['text'])
+#                 await self.sender.sendMessage('Got ' + msg['text'][0:40] + '...')
+#
+#             elif command == 'help':
+#                 self.logger.info(str(sender_id) + ' ' + "Hello! Send a magnet link you want to save.")
+#                 await self.sender.sendMessage("Hello! Send a magnet link you want to save.")
+#
+#             else:
+#                 self.logger.info(str(sender_id) + ' ' + "I don't know this command, try to use `help` command")
+#                 await self.sender.sendMessage("I don't know this command, try to use `help` command")
+#         else:
+#             # No reply to user, (401) Unauthorized
+#             self.logger.warn('Unknown user sent message')
+
+
+class Bot(InlineUserHandler, AnswererMixin):
+    # Setup Bot:
+    TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    approved_ids = os.environ.get('APPROVED_IDS').split(',')
+    logger = logging.getLogger(TELEGRAM_LOGGER)
+
     def __init__(self, *args, **kwargs):
-        super(DirectMsgHandler, self).__init__(*args, **kwargs)
-        self.logger = logging.getLogger(TELEGRAM_LOGGER)
-
-    async def on_chat_message(self, msg):
-        sender_id = msg['from']['id']
-        self.logger.debug('sender_id: ' + str(sender_id))
-        self.logger.info("Incoming message:")
-        self.logger.info(msg)
-
-        if str(sender_id) in self.approved_ids:
-            self.logger.debug('Known user sent a message.')
-
-            command = msg['text']
-            self.logger.debug('Command is ' + command)
-
-            if 'magnet' in command:
-                create_torrent(msg['text'], msg['date'])
-                self.logger.info(str(sender_id) + ' ' + msg['text'])
-                await self.sender.sendMessage('Got ' + msg['text'][0:40] + '...')
-
-            elif command == 'help':
-                self.logger.info(str(sender_id) + ' ' + "Hello! Send a magnet link you want to save.")
-                await self.sender.sendMessage("Hello! Send a magnet link you want to save.")
-
-            else:
-                self.logger.info(str(sender_id) + ' ' + "I don't know this command, try to use `help` command")
-                await self.sender.sendMessage("I don't know this command, try to use `help` command")
-        else:
-            # No reply to user, (401) Unauthorized
-            self.logger.warn('Unknown user sent message')
-
-
-class Bot:
-    def __init__(self):
-        # Setup Bot:
-        self.TOKEN = os.environ.get('TELEGRAM_TOKEN')
-        self.approved_ids = os.environ.get('APPROVED_IDS').split(',')
-        self.logger = logging.getLogger(TELEGRAM_LOGGER)
+        super(Bot, self).__init__(*args, **kwargs)
         print('start bot')
 
+    # @staticmethod
+    def extract_btih(text):
+        pattern = re.compile('magnet:\?xt=urn:btih:([^&/]+)')
+        matches = pattern.match(text)
+        return matches.groups(1)[0]
+
     # Returns [{article},{article},]
+    # @staticmethod
     def build_articles(search_phrase):
         articles = []
         for magnet in Scraper.request_magnet_links(search_phrase):
             articles += [{
-                'id': extract_btih(magnet['magnet_link']),  # Needs to be unique
+                'id': Bot.extract_btih(magnet['magnet_link']),  # Needs to be unique
                 'type': 'article',
                 'title': magnet['title'],
                 'message_text': magnet['magnet_link'],
@@ -98,15 +107,9 @@ class Bot:
             }]
         return articles
 
-
-    def extract_btih(text):
-        pattern = re.compile('magnet:\?xt=urn:btih:([^&/]+)')
-        matches = pattern.match(text)
-        return matches.groups(1)[0]
-
-
+    # @staticmethod
     def create_magnet_articles(query_string):
-        return build_articles(query_string)
+        return Bot.build_articles(query_string)
 
 
     # Articles:
@@ -118,16 +121,29 @@ class Bot:
     # article type https://core.telegram.org/bots/api#inline-mode
 
     def create_torrent(text, date):
-        torrent_file_hash = extract_btih(text)  # This isn't needed, just a sanity to verify input.
+        torrent_file_hash = Bot.extract_btih(text)  # This isn't needed, just a sanity to verify input.
         if torrent_file_hash:
             output_path = os.path.join(os.environ.get('OUTPUT_DIR'), 'meta-' + str(date) + '.torrent')
             f1 = open(output_path, 'w+')
             f1.write('d10:magnet-uri' + str(len(text)) + ':' + text + 'e')
             f1.close()
-            self.logger.info('Created file: ' + output_path)
+            Bot.logger.info('Created file: ' + output_path)
             # return success msg
         else:
-            self.logger.info('No magnet matched. :-(')
+            Bot.logger.info('No magnet matched. :-(')
+
+    def on_inline_query(self, msg):
+        def compute_answer():
+            query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
+            # print(self.id, ':', 'Inline Query:', query_id, from_id, query_string)
+            articles = Bot.create_magnet_articles(query_string)
+            return articles
+
+        self.answerer.answer(msg, compute_answer)
+
+    def on_chosen_inline_result(self, msg):
+        result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
+        print(self.id, ':', 'Chosen Inline Result:', result_id, from_id, query_string)
 
     def start(self):
         bot = telepot.aio.DelegatorBot(self.TOKEN, [
