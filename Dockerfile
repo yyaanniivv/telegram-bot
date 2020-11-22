ddea93edcc7de1c8f62efdbe7e7c2b884af8752f
Yaniv M <yyaanniivv@users.noreply.github.com>
2020-11-22 16:15:28 +0200

    Initial bot response

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,4 @@ ADD . /app
 WORKDIR /app
 RUN pip install --upgrade pip && pip install -r ./requirements.txt
 
-# EXPOSE 80 ?
 CMD ["python", "app.py"]
-
--- app.py
+++ app.py
@@ -1,12 +1,24 @@
+from os.path import dirname, join, os
+
+from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import CallbackContext, CommandHandler, Updater
 
 
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-updater = Updater('YOUR TOKEN HERE')
+# __MAIN__
+# Load .env params:
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Setup Bot:
+TOKEN = os.environ.get('TELEGRAM_TOKEN')
+approved_ids = os.environ.get('APPROVED_IDS').split(',')
+
+updater = Updater(TOKEN)
 
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
